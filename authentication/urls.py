5f1e65b6d8335f5110f3fc0e0359312d800da117
Eric <ericjesus403@gmail.com>
2024-02-06 15:40:19 -0300

    feat: Implementando authentication na api

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path('authentication/token/', TokenObtainPairView.as_view(),
+         name='authentication_obtain_token'),
+    path('authentication/refresh/token/', TokenRefreshView.as_view(),
+         name='authentication_refresh_token'),
+    path('authentication/verify/token/', TokenVerifyView.as_view(),
+         name='authentication_verify_token'),
+]
